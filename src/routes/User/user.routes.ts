8b213980d0e94d45f8dc89e0d3c7a660972df1d4
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2022-11-01 14:43:41 -0300

    feat: user routes and validations

--- src/routes/User/user.routes.ts
+++ src/routes/User/user.routes.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import { userRegisterValidation } from "./user.validations";
 
 const router = Router();
+
+router.post('/', userRegisterValidation())
 
 export default router;
 
