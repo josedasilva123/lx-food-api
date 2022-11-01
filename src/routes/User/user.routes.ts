7fd5032523a3d7afa46325bd6981955615efd5ed
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2022-11-01 16:00:24 -0300

    feat: finishing user routes (implementing in index file)

--- src/routes/User/user.routes.ts
+++ src/routes/User/user.routes.ts
@@ -1,7 +1,15 @@
 import { Router } from "express";
-import { userRegisterValidation } from "./user.validations";
+import { Authenticate } from "../../middlewares/authenticate";
+import { HandleErrors } from "../../middlewares/handleErrors";
+import { Validate } from "../../middlewares/handleValidation";
+import UserControllers from "./user.controllers";
+import { userLoginValidation, userRegisterValidation } from "./user.validations";
 
 const router = Router();
+
+router.post('/', userRegisterValidation(), Validate, HandleErrors(UserControllers.Register));
+router.post('/login', userLoginValidation(), Validate, HandleErrors(UserControllers.Login));
+router.get('/autologin', Authenticate, HandleErrors(UserControllers.AutoLogin));
 
 export default router;
 
