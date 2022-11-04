d4c236a51c7b442a9356f4eeccc50117c3bad8df
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2022-11-04 10:34:04 -0300

    fix: identation

--- src/services/Recipe/Get.service.ts
+++ src/services/Recipe/Get.service.ts
@@ -4,7 +4,7 @@ import { iGlobalRecipeQuery, iRecipeGetParams } from "../../routes/Recipe/types"
 
 export class RecipeGet {
     async execute(params: iRecipeGetParams, query: iGlobalRecipeQuery) {
-      const { userId,  } = params;
+      const { userId } = params;
       const { limit, skip } = query;
   
       const newQuery = userId ? { _id: new ObjectId(userId)} : {};
