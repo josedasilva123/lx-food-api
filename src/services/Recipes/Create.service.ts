18185f095176eef47102c6a3fd4158ac8e3980cb
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2022-11-04 09:58:08 -0300

    feat: moving file validation to controllers

--- src/services/Recipes/Create.service.ts
+++ src/services/Recipes/Create.service.ts
@@ -1,14 +1,10 @@
 import Recipe from "../../models/Recipes";
-import { iRecipeCreateBody } from "../../routes/Recipes/types";
+import { iRecipeCreateBody } from "../../routes/Recipe/types";
 import { Image } from "../File/Image";
 
 export class RecipeCreate{
     async execute(body: iRecipeCreateBody, file: Express.Multer.File){
         const { userID, title, content, categories } = body;
-
-        if(!file) {
-            throw new Error("Arquivo enviando inválido."); 
-        }
 
         const image = new Image();
         
