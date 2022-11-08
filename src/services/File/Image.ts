b5e851ddab08dbe3b0aa7a563469a51565a44b0e
Alex Conder <alex.v.conder@gmail.com>
2022-11-08 14:44:10 -0300

    fix: fixing sharp cache

--- src/services/File/Image.ts
+++ src/services/File/Image.ts
@@ -1,16 +1,19 @@
+import { unlinkSync } from "fs";
 import sharp from "sharp";
 import { File } from "./File";
 
-export class Image extends File{
-    async optmize(file: Express.Multer.File){
-        await sharp(file.path)
-        .resize({
-            fit: sharp.fit.contain,
-            width: 1000,
-        })
-        .webp({ quality: 50 })
-        .toFile(`uploads/webp/${file.filename}.webp`);
+export class Image extends File {
+  async optmize(file: Express.Multer.File) {
+    await sharp(file.path, {})
+      .resize({
+        fit: sharp.fit.contain,
+        width: 1000,
+      })
+      .webp({ quality: 50 })
+      .toFile(`uploads/webp/${file.filename}.webp`);
 
-        return { path: `${process.env.BASE_URL}/uploads/webp/${file.filename}`}
-    }
+    sharp.cache({ files: 0 });
+
+    return { path: `${process.env.BASE_URL}/uploads/webp/${file.filename}` };
+  }
 }
