64fd88af27feb5af524d4e6685a56e8da15ee949
Alex Conder <alex.v.conder@gmail.com>
2022-11-08 14:40:55 -0300

    identation

--- src/services/File/File.ts
+++ src/services/File/File.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 export class File{
     async delete(path: string){
         fs.unlink(path, () => {
-            console.log("Arquivo deletado com sucesso!")
-        })
+            console.log('Arquivo deletado com sucesso!');
+        });
     }
 }
