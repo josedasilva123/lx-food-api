f9bcc0e6e532adf33a2d9980fc6765de3b218b90
Alex Conder <alex.v.conder@gmail.com>
2022-11-24 19:51:51 -0300

    fix: function to let (cause is a class not a functional syntax)

--- src/services/Recipe/Get.service.ts
+++ src/services/Recipe/Get.service.ts
@@ -6,20 +6,20 @@ export class RecipeGet {
       const { userId } = params;
       const { category, search, limit, skip } = query;
 
-      const newQuery = () => {
-         const categoryQuery = category ? { categories: [category] } : {};
+      let newQuery;
 
-         if (userId) {
-            return { ...categoryQuery, userID: userId };
-         } else if (search) {
-            const searchRegex = new RegExp(search, "i");
-            return { ...categoryQuery, title: { $regex: searchRegex } };
-         } else if (category) {
-            return categoryQuery;
-         } else {
-            return undefined;
-         }
-      };
+      const categoryQuery = category ? { categories: [category] } : {};
+
+      if (userId) {
+         newQuery = { ...categoryQuery, userID: userId };
+      } else if (search) {
+         const searchRegex = new RegExp(search, "i");
+         newQuery = { ...categoryQuery, title: { $regex: searchRegex } };
+      } else if (category) {
+         newQuery = categoryQuery;
+      } else {
+         newQuery = undefined;
+      }
 
       const count = (await Recipe.find(newQuery as object)).length;
 
