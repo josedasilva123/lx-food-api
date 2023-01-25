5584a74ae5d905018a907bc47ebf6d3e2f233ff2
Alex Conder <alex.v.conder@gmail.com>
2023-01-25 09:31:55 -0300

    feat: review edit service

---
+++ src/services/Reviews/Edit.service.ts
@@ -0,0 +1,51 @@
+import { ObjectId } from "mongodb";
+import Recipe from "../../models/Recipes";
+export class RecipeCreate {
+   async execute(body: any, params: any) {
+      const { recipeId, reviewId } = params;
+      const { id, content, score } = body;
+
+      const newReview = {
+         content,
+         score,
+      };
+
+      const recipeObjectId = new ObjectId(recipeId);
+
+      const recipe = await Recipe.findOne({ _id: recipeObjectId });
+
+      if (!recipe) {
+         throw new Error("A receita que você referenciou não existe.");
+      }
+
+      const reviewList = recipe?.reviews ? recipe?.reviews : [];
+
+      const review = reviewList.find(review => review._id === reviewId);
+
+      if(!review){
+        throw new Error("A revisão que você está tentando editar não existe.")
+      }
+
+      if(review.userId !== id){
+        throw new Error("Somente o autor da revisão pode edita-la.")
+      } 
+      const newReviewsList = reviewList.map(review => {
+        if(review._id !== reviewId){
+            return { ...review, ...newReview};
+        } else {
+            return review;
+        }
+      })
+
+      await Recipe.updateOne(
+         { _id: recipeObjectId },
+         {
+            $set: {
+               reviews: newReviewsList,
+            },
+         }
+      );
+
+      return { message: "Revisão cadastrada com sucesso!" };
+   }
+}
